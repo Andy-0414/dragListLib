3b2e37336ca0ead02bddceaf9711b1cd8364050f
Andy0414 <pjh8667@naver.com>
2019-06-04 19:38:15 +0900

    CREATE rp

---
+++ dist/draglist.js
@@ -0,0 +1,43 @@
+(() => {
+    var dragList = document.getElementsByClassName("drag-list")
+        ;[...dragList].forEach(drag => {
+            drag.style.position = "relative"
+
+            var target = null
+            var itemHeight = drag.children[0].scrollHeight
+
+            var tmpElement = null
+            drag.addEventListener("mousedown", e => {
+                target = e.target
+                var selectIndex = [...drag.children].findIndex(x =>x==target)
+
+                tmpElement = target.cloneNode("true")
+                tmpElement.style.opacity = 0.2
+                drag.insertBefore(tmpElement, drag.children[selectIndex])
+                
+                target.style.position = "absolute"
+                target.style.top = e.clientY - drag.getBoundingClientRect().top - target.clientHeight / 2 + "px"
+            })
+            drag.addEventListener("mousemove", e => {
+                if (target) {
+                    var itemIndex = Math.floor((e.clientY - drag.getBoundingClientRect().top + itemHeight/2) / itemHeight)
+                    drag.removeChild(tmpElement)
+                    drag.insertBefore(tmpElement, drag.children[itemIndex])
+
+                    target.style.top = e.clientY - drag.getBoundingClientRect().top - target.clientHeight / 2 + "px"
+                    target.style.boxShadow = "0px 5px 10px 0px #AAAAAAAA"
+                }
+            })
+            drag.addEventListener("mouseup", e => {
+                var itemIndex = Math.floor((e.clientY - drag.getBoundingClientRect().top + itemHeight / 2) / itemHeight)
+                drag.removeChild(tmpElement)
+                drag.insertBefore(target, drag.children[itemIndex])
+
+                target.style.position="static"
+                target.style.boxShadow = "none"
+
+                tmpElement = null
+                target = null
+            })
+        })
+})()
